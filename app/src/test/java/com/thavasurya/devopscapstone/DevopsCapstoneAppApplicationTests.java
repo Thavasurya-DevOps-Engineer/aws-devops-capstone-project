87571c2f0cfbcc3dc7331a69e9e5bf596ac28910
Ubuntu <ubuntu@ip-10-0-1-96.ap-south-1.compute.internal>
2026-07-29 20:14:12 +0000

    Initial commit - DevOps Capstone Spring Boot Application

---
+++ app/src/test/java/com/thavasurya/devopscapstone/DevopsCapstoneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thavasurya.devopscapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsCapstoneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
